23e73fc254ddb4d8f9cd73217b5801b403fbf5d5
sen18012 <66652098+sen18012@users.noreply.github.com>
2021-04-22 00:31:18 -0600

    Buzzer terminado

--- Proyecto_3/Buzzer/Buzzer.ino
+++ Proyecto_3/Buzzer/Buzzer.ino
@@ -12,129 +12,116 @@
  
  by Dung Dang
  */
+#include "pitches.h"
+#define REST 0
 
-#define note_cc 261
-#define note_dd 294
-#define note_ee 329
-#define note_ff 349
-#define note_g 391
-#define note_gS 415
-#define note_a 440
-#define note_aS 455
-#define note_b 466
-#define note_cH 523
-#define note_cSH 554
-#define note_dH 587
-#define note_dSH 622
-#define note_eH 659
-#define note_fH 698
-#define note_fSH 740
-#define note_gH 784
-#define note_gSH 830
-#define note_aH 880
 
-int buzzerPin = 40;
+// change this to make the song slower or faster
+int tempo = 140;
 
-void beep(int note, int duration)
-{
-  tone(buzzerPin, note, duration/2);
-  delay(duration/2);
-  noTone(buzzerPin);
-  delay(duration/2 + 20);  
+// change this to whichever pin you want to use
+int buzzer = 40;
+
+// notes of the moledy followed by the duration.
+// a 4 means a quarter note, 8 an eighteenth , 16 sixteenth, so on
+// !!negative numbers are used to represent dotted notes,
+// so -4 means a dotted quarter note, that is, a quarter plus an eighteenth!!
+int melody[] = {
+
+  // Gren Hill Zone - Sonic the Hedgehog
+  // Score available at https://musescore.com/user/248346/scores/461661
+  // Theme by Masato Nakamura, arranged by Teddy Mason
+  
+  REST,2, NOTE_D5,8, NOTE_B4,4, NOTE_D5,8, //1
+  NOTE_CS5,4, NOTE_D5,8, NOTE_CS5,4, NOTE_A4,2, 
+  REST,8, NOTE_A4,8, NOTE_FS5,8, NOTE_E5,4, NOTE_D5,8,
+  NOTE_CS5,4, NOTE_D5,8, NOTE_CS5,4, NOTE_A4,2, 
+  REST,4, NOTE_D5,8, NOTE_B4,4, NOTE_D5,8,
+  NOTE_CS5,4, NOTE_D5,8, NOTE_CS5,4, NOTE_A4,2, 
+
+  REST,8, NOTE_B4,8, NOTE_B4,8, NOTE_G4,4, NOTE_B4,8, //7
+  NOTE_A4,4, NOTE_B4,8, NOTE_A4,4, NOTE_D4,2,
+  REST,4, NOTE_D5,8, NOTE_B4,4, NOTE_D5,8,
+  NOTE_CS5,4, NOTE_D5,8, NOTE_CS5,4, NOTE_A4,2, 
+  REST,8, NOTE_A4,8, NOTE_FS5,8, NOTE_E5,4, NOTE_D5,8,
+  NOTE_CS5,4, NOTE_D5,8, NOTE_CS5,4, NOTE_A4,2, 
+
+  REST,4, NOTE_D5,8, NOTE_B4,4, NOTE_D5,8, //13
+  NOTE_CS5,4, NOTE_D5,8, NOTE_CS5,4, NOTE_A4,2, 
+  REST,8, NOTE_B4,8, NOTE_B4,8, NOTE_G4,4, NOTE_B4,8,
+  NOTE_A4,4, NOTE_B4,8, NOTE_A4,4, NOTE_D4,8, NOTE_D4,8, NOTE_FS4,8,
+  NOTE_E4,-1,
+  REST,8, NOTE_D4,8, NOTE_E4,8, NOTE_FS4,-1,
+
+  REST,8, NOTE_D4,8, NOTE_D4,8, NOTE_FS4,8, NOTE_F4,-1, //20
+  REST,8, NOTE_D4,8, NOTE_F4,8, NOTE_E4,-1, //end 1
+
+  //repeats from 1
+
+  REST,2, NOTE_D5,8, NOTE_B4,4, NOTE_D5,8, //1
+  NOTE_CS5,4, NOTE_D5,8, NOTE_CS5,4, NOTE_A4,2, 
+  REST,8, NOTE_A4,8, NOTE_FS5,8, NOTE_E5,4, NOTE_D5,8,
+  NOTE_CS5,4, NOTE_D5,8, NOTE_CS5,4, NOTE_A4,2, 
+  REST,4, NOTE_D5,8, NOTE_B4,4, NOTE_D5,8,
+  NOTE_CS5,4, NOTE_D5,8, NOTE_CS5,4, NOTE_A4,2, 
+
+  REST,8, NOTE_B4,8, NOTE_B4,8, NOTE_G4,4, NOTE_B4,8, //7
+  NOTE_A4,4, NOTE_B4,8, NOTE_A4,4, NOTE_D4,2,
+  REST,4, NOTE_D5,8, NOTE_B4,4, NOTE_D5,8,
+  NOTE_CS5,4, NOTE_D5,8, NOTE_CS5,4, NOTE_A4,2, 
+  REST,8, NOTE_A4,8, NOTE_FS5,8, NOTE_E5,4, NOTE_D5,8,
+  NOTE_CS5,4, NOTE_D5,8, NOTE_CS5,4, NOTE_A4,2, 
+
+  REST,4, NOTE_D5,8, NOTE_B4,4, NOTE_D5,8, //13
+  NOTE_CS5,4, NOTE_D5,8, NOTE_CS5,4, NOTE_A4,2, 
+  REST,8, NOTE_B4,8, NOTE_B4,8, NOTE_G4,4, NOTE_B4,8,
+  NOTE_A4,4, NOTE_B4,8, NOTE_A4,4, NOTE_D4,8, NOTE_D4,8, NOTE_FS4,8,
+  NOTE_E4,-1,
+  REST,8, NOTE_D4,8, NOTE_E4,8, NOTE_FS4,-1,
+
+  REST,8, NOTE_D4,8, NOTE_D4,8, NOTE_FS4,8, NOTE_F4,-1, //20
+  REST,8, NOTE_D4,8, NOTE_F4,8, NOTE_E4,8, //end 2
+  NOTE_E4,-2, NOTE_A4,8, NOTE_CS5,8, 
+  NOTE_FS5,8, NOTE_E5,4, NOTE_D5,8, NOTE_A5,-4,
+
+};
+
+// sizeof gives the number of bytes, each int value is composed of two bytes (16 bits)
+// there are two values per note (pitch and duration), so for each note there are four bytes
+int notes = sizeof(melody) / sizeof(melody[0]) / 2;
+
+// this calculates the duration of a whole note in ms
+int wholenote = (60000 * 4) / tempo;
+
+int divider = 0, noteDuration = 0;
+
+void setup() {
+  // iterate over the notes of the melody.
+  // Remember, the array is twice the number of notes (notes + durations)
+  for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
+
+    // calculates the duration of each note
+    divider = melody[thisNote + 1];
+    if (divider > 0) {
+      // regular note, just proceed
+      noteDuration = (wholenote) / divider;
+    } else if (divider < 0) {
+      // dotted notes are represented with negative durations!!
+      noteDuration = (wholenote) / abs(divider);
+      noteDuration *= 1.5; // increases the duration in half for dotted notes
+    }
+
+    // we only play the note for 90% of the duration, leaving 10% as a pause
+    tone(buzzer, melody[thisNote], noteDuration * 0.9);
+
+    // Wait for the specief duration before playing the next note.
+    delay(noteDuration);
+
+    // stop the waveform generation before the next note.
+    noTone(buzzer);
+  }
 }
-void setup() 
-{
-  pinMode(buzzerPin,OUTPUT);
-}
-void loop() 
-{
-  beep(note_a, 500);
-  beep(note_a, 500);
-  beep(note_a, 500);
-  beep(note_ff, 350);
-  beep(note_cH, 150);  
-  beep(note_a, 500);
-  beep(note_ff, 350);
-  beep(note_cH, 150);
-  beep(note_a, 650);
 
-  delay(150);
-  //end of first bit   
-
-  beep(note_eH, 500);
-  beep(note_eH, 500);
-  beep(note_eH, 500);   
-  beep(note_fH, 350);
-  beep(note_cH, 150);
-  beep(note_gS, 500);
-  beep(note_ff, 350);
-  beep(note_cH, 150);
-  beep(note_a, 650);
-
-  delay(150);
-  //end of second bit...  
-
-  beep(note_aH, 500);
-  beep(note_a, 300);
-  beep(note_a, 150);
-  beep(note_aH, 400);
-  beep(note_gSH, 200);
-  beep(note_gH, 200); 
-  beep(note_fSH, 125);
-  beep(note_fH, 125);    
-  beep(note_fSH, 250);
-
-  delay(250);
-
-  beep(note_aS, 250); 
-  beep(note_dSH, 400); 
-  beep(note_dH, 200);  
-  beep(note_cSH, 200);  
-  beep(note_cH, 125);  
-  beep(note_b, 125);  
-  beep(note_cH, 250);  
-
-  delay(250);
-
-  beep(note_ff, 125);  
-  beep(note_gS, 500);  
-  beep(note_ff, 375);  
-  beep(note_a, 125);
-  beep(note_cH, 500);
-  beep(note_a, 375);  
-  beep(note_cH, 125);
-  beep(note_eH, 650);
-
-
-  beep(note_aH, 500);
-  beep(note_a, 300);
-  beep(note_a, 150);
-  beep(note_aH, 400);
-  beep(note_gSH, 200);
-  beep(note_gH, 200);
-  beep(note_fSH, 125);
-  beep(note_fH, 125);    
-  beep(note_fSH, 250);
-
-  delay(250);
-
-  beep(note_aS, 250);  
-  beep(note_dSH, 400);  
-  beep(note_dH, 200);  
-  beep(note_cSH, 200);  
-  beep(note_cH, 125);  
-  beep(note_b, 125);  
-  beep(note_cH, 250);      
-
-  delay(250);
-
-  beep(note_ff, 250);  
-  beep(note_gS, 500);  
-  beep(note_ff, 375);  
-  beep(note_cH, 125);
-  beep(note_a, 500);   
-  beep(note_ff, 375);   
-  beep(note_cH, 125); 
-  beep(note_a, 650);   
-  //end of the song
+void loop() {
+  // no need to repeat the melody.
 }
