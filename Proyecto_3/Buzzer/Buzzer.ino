646b50eb4776e3030b8a00796695fdb04d6ad6fc
sen18012 <66652098+sen18012@users.noreply.github.com>
2021-04-24 14:54:49 -0600

    Movimientos hasta posición 18 en J1 Mapa 1

--- Proyecto_3/Buzzer/Buzzer.ino
+++ Proyecto_3/Buzzer/Buzzer.ino
@@ -1,86 +1,82 @@
 /*
- Educational BoosterPack MK II - Imperial March Tune
- http://boosterpackdepot.info/wiki/index.php?title=Educational_BoosterPack_MK_II
- 
- 
- Based on the Imperial March implementation on MSP430 LaunchPad
- posted here http://processors.wiki.ti.com/index.php/Playing_The_Imperial_March
- Originally created for Educational BoosterPack 
- Buzzer Pin was 19
- Modified for Educational BoosterPack MK II - Dec 03 2013
- Buzzer Pin = 40
- 
- by Dung Dang
+ * ------ Proyecto # 3 ------ 
+ * Natalia de León 18193
+ * Katharine Senn 18012
+ * Sección 20
+ * Abril 2021
  */
+
+ //***************************************************************************************************************************************
+/* Código para reproducción de audio en Buzzer para TivaC
+ * Basado en el código de Dung Dang - https://github.com/robsoncouto/arduino-songs/blob/master/greenhill/greenhill.ino //https://musescore.com/user/248346/scores/461661
+ * y el ejemplo de energia BuzzerImperialMarchTune
+ * Adaptación para reproducción de canción Green Hill Zone - Sonic the Hedgehog
+ * Modificado por Natalia de León y Katharine Senn
+ * IE3027: Electrónica Digital 2 - 2021
+ */
+//***************************************************************************************************************************************
+
 #include "pitches.h"
-#define REST 0
-
-
-// change this to make the song slower or faster
-int tempo = 140;
-
-// change this to whichever pin you want to use
-int buzzer = 40;
+int tempo = 140; //tiempo de la música (Para acelerar o alentar la canción)
+int buzzer = 40; //Pin para conectar el buzzer
+const int buttonPin = PUSH2;  //Pin con el que se inicia la reproducción
+int buttonState = 0; 
+int si = 0;
 
 // notes of the moledy followed by the duration.
 // a 4 means a quarter note, 8 an eighteenth , 16 sixteenth, so on
 // !!negative numbers are used to represent dotted notes,
 // so -4 means a dotted quarter note, that is, a quarter plus an eighteenth!!
 int melody[] = {
-
-  // Gren Hill Zone - Sonic the Hedgehog
-  // Score available at https://musescore.com/user/248346/scores/461661
-  // Theme by Masato Nakamura, arranged by Teddy Mason
-  
-  REST,2, NOTE_D5,8, NOTE_B4,4, NOTE_D5,8, //1
+  NOTE_NOTONE,2, NOTE_D5,8, NOTE_B4,4, NOTE_D5,8, //1
   NOTE_CS5,4, NOTE_D5,8, NOTE_CS5,4, NOTE_A4,2, 
-  REST,8, NOTE_A4,8, NOTE_FS5,8, NOTE_E5,4, NOTE_D5,8,
+  NOTE_NOTONE,8, NOTE_A4,8, NOTE_FS5,8, NOTE_E5,4, NOTE_D5,8,
   NOTE_CS5,4, NOTE_D5,8, NOTE_CS5,4, NOTE_A4,2, 
-  REST,4, NOTE_D5,8, NOTE_B4,4, NOTE_D5,8,
+  NOTE_NOTONE,4, NOTE_D5,8, NOTE_B4,4, NOTE_D5,8,
   NOTE_CS5,4, NOTE_D5,8, NOTE_CS5,4, NOTE_A4,2, 
 
-  REST,8, NOTE_B4,8, NOTE_B4,8, NOTE_G4,4, NOTE_B4,8, //7
+  NOTE_NOTONE,8, NOTE_B4,8, NOTE_B4,8, NOTE_G4,4, NOTE_B4,8, //7
   NOTE_A4,4, NOTE_B4,8, NOTE_A4,4, NOTE_D4,2,
-  REST,4, NOTE_D5,8, NOTE_B4,4, NOTE_D5,8,
+  NOTE_NOTONE,4, NOTE_D5,8, NOTE_B4,4, NOTE_D5,8,
   NOTE_CS5,4, NOTE_D5,8, NOTE_CS5,4, NOTE_A4,2, 
-  REST,8, NOTE_A4,8, NOTE_FS5,8, NOTE_E5,4, NOTE_D5,8,
+  NOTE_NOTONE,8, NOTE_A4,8, NOTE_FS5,8, NOTE_E5,4, NOTE_D5,8,
   NOTE_CS5,4, NOTE_D5,8, NOTE_CS5,4, NOTE_A4,2, 
 
-  REST,4, NOTE_D5,8, NOTE_B4,4, NOTE_D5,8, //13
+  NOTE_NOTONE,4, NOTE_D5,8, NOTE_B4,4, NOTE_D5,8, //13
   NOTE_CS5,4, NOTE_D5,8, NOTE_CS5,4, NOTE_A4,2, 
-  REST,8, NOTE_B4,8, NOTE_B4,8, NOTE_G4,4, NOTE_B4,8,
+  NOTE_NOTONE,8, NOTE_B4,8, NOTE_B4,8, NOTE_G4,4, NOTE_B4,8,
   NOTE_A4,4, NOTE_B4,8, NOTE_A4,4, NOTE_D4,8, NOTE_D4,8, NOTE_FS4,8,
   NOTE_E4,-1,
-  REST,8, NOTE_D4,8, NOTE_E4,8, NOTE_FS4,-1,
+  NOTE_NOTONE,8, NOTE_D4,8, NOTE_E4,8, NOTE_FS4,-1,
 
-  REST,8, NOTE_D4,8, NOTE_D4,8, NOTE_FS4,8, NOTE_F4,-1, //20
-  REST,8, NOTE_D4,8, NOTE_F4,8, NOTE_E4,-1, //end 1
+  NOTE_NOTONE,8, NOTE_D4,8, NOTE_D4,8, NOTE_FS4,8, NOTE_F4,-1, //20
+  NOTE_NOTONE,8, NOTE_D4,8, NOTE_F4,8, NOTE_E4,-1, //end 1
 
   //repeats from 1
 
-  REST,2, NOTE_D5,8, NOTE_B4,4, NOTE_D5,8, //1
+  NOTE_NOTONE,2, NOTE_D5,8, NOTE_B4,4, NOTE_D5,8, //1
   NOTE_CS5,4, NOTE_D5,8, NOTE_CS5,4, NOTE_A4,2, 
-  REST,8, NOTE_A4,8, NOTE_FS5,8, NOTE_E5,4, NOTE_D5,8,
+  NOTE_NOTONE,8, NOTE_A4,8, NOTE_FS5,8, NOTE_E5,4, NOTE_D5,8,
   NOTE_CS5,4, NOTE_D5,8, NOTE_CS5,4, NOTE_A4,2, 
-  REST,4, NOTE_D5,8, NOTE_B4,4, NOTE_D5,8,
+  NOTE_NOTONE,4, NOTE_D5,8, NOTE_B4,4, NOTE_D5,8,
   NOTE_CS5,4, NOTE_D5,8, NOTE_CS5,4, NOTE_A4,2, 
 
-  REST,8, NOTE_B4,8, NOTE_B4,8, NOTE_G4,4, NOTE_B4,8, //7
+  NOTE_NOTONE,8, NOTE_B4,8, NOTE_B4,8, NOTE_G4,4, NOTE_B4,8, //7
   NOTE_A4,4, NOTE_B4,8, NOTE_A4,4, NOTE_D4,2,
-  REST,4, NOTE_D5,8, NOTE_B4,4, NOTE_D5,8,
+  NOTE_NOTONE,4, NOTE_D5,8, NOTE_B4,4, NOTE_D5,8,
   NOTE_CS5,4, NOTE_D5,8, NOTE_CS5,4, NOTE_A4,2, 
-  REST,8, NOTE_A4,8, NOTE_FS5,8, NOTE_E5,4, NOTE_D5,8,
+  NOTE_NOTONE,8, NOTE_A4,8, NOTE_FS5,8, NOTE_E5,4, NOTE_D5,8,
   NOTE_CS5,4, NOTE_D5,8, NOTE_CS5,4, NOTE_A4,2, 
 
-  REST,4, NOTE_D5,8, NOTE_B4,4, NOTE_D5,8, //13
+  NOTE_NOTONE,4, NOTE_D5,8, NOTE_B4,4, NOTE_D5,8, //13
   NOTE_CS5,4, NOTE_D5,8, NOTE_CS5,4, NOTE_A4,2, 
-  REST,8, NOTE_B4,8, NOTE_B4,8, NOTE_G4,4, NOTE_B4,8,
+  NOTE_NOTONE,8, NOTE_B4,8, NOTE_B4,8, NOTE_G4,4, NOTE_B4,8,
   NOTE_A4,4, NOTE_B4,8, NOTE_A4,4, NOTE_D4,8, NOTE_D4,8, NOTE_FS4,8,
   NOTE_E4,-1,
-  REST,8, NOTE_D4,8, NOTE_E4,8, NOTE_FS4,-1,
+  NOTE_NOTONE,8, NOTE_D4,8, NOTE_E4,8, NOTE_FS4,-1,
 
-  REST,8, NOTE_D4,8, NOTE_D4,8, NOTE_FS4,8, NOTE_F4,-1, //20
-  REST,8, NOTE_D4,8, NOTE_F4,8, NOTE_E4,8, //end 2
+  NOTE_NOTONE,8, NOTE_D4,8, NOTE_D4,8, NOTE_FS4,8, NOTE_F4,-1, //20
+  NOTE_NOTONE,8, NOTE_D4,8, NOTE_F4,8, NOTE_E4,8, //end 2
   NOTE_E4,-2, NOTE_A4,8, NOTE_CS5,8, 
   NOTE_FS5,8, NOTE_E5,4, NOTE_D5,8, NOTE_A5,-4,
 
@@ -96,7 +92,49 @@ int wholenote = (60000 * 4) / tempo;
 int divider = 0, noteDuration = 0;
 
 void setup() {
-  // iterate over the notes of the melody.
+  //pinMode(buttonPin, INPUT_PULLUP);
+
+//  // iterate over the notes of the melody.
+//  // Remember, the array is twice the number of notes (notes + durations)
+//  for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
+//
+//    // calculates the duration of each note
+//    divider = melody[thisNote + 1];
+//    if (divider > 0) {
+//      // regular note, just proceed
+//      noteDuration = (wholenote) / divider;
+//    } else if (divider < 0) {
+//      // dotted notes are represented with negative durations!!
+//      noteDuration = (wholenote) / abs(divider);
+//      noteDuration *= 1.5; // increases the duration in half for dotted notes
+//    }
+//
+//    // we only play the note for 90% of the duration, leaving 10% as a pause
+//    tone(buzzer, melody[thisNote], noteDuration * 0.9);
+//
+//    // Wait for the specief duration before playing the next note.
+//    delay(noteDuration);
+//
+//    // stop the waveform generation before the next note.
+//    noTone(buzzer);
+//  }
+}
+
+void loop() {
+
+    buttonState = digitalRead(buttonPin);
+    
+  if (buttonState == HIGH) {     
+    // turn LED on:    
+    si = 1;  
+  } 
+  else {
+    // turn LED off:
+    si = 0;; 
+  }
+  
+ while (si == 0){
+    // iterate over the notes of the melody.
   // Remember, the array is twice the number of notes (notes + durations)
   for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
 
@@ -119,9 +157,8 @@ void setup() {
 
     // stop the waveform generation before the next note.
     noTone(buzzer);
-  }
-}
-
-void loop() {
   // no need to repeat the melody.
-}
+  }
+  }
+
+  }
