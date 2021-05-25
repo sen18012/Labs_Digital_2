df3e1b490855d651657f5a1ac32343143961d04e
sen18012 <66652098+sen18012@users.noreply.github.com>
2021-05-25 00:19:48 -0600

    UART_tiva y web server

--- Proyecto_4/WebServer/WebServer.ino
+++ Proyecto_4/WebServer/WebServer.ino
@@ -23,8 +23,8 @@ const char* password = "7FdCe2f9f8";  //Enter your Password here
 WebServer server(80);  // Object of WebServer(HTTP port, 80 is defult)
 
 
-uint8_t LED1pin = 2;
-int Val = 0;
+//uint8_t LED1pin = 2;
+int parqueos = 0;
 bool LED1status = LOW;
 
 //************************************************************************************************
@@ -38,7 +38,7 @@ void setup() {
   Serial2.begin(115200, SERIAL_8N1, 16, 17);
 
 
-  pinMode(LED1pin, OUTPUT);
+//  pinMode(LED1pin, OUTPUT);
 
   // Connect to your wi-fi modem
   WiFi.begin(ssid, password);
@@ -54,8 +54,8 @@ void setup() {
   Serial.println(WiFi.localIP());  //Show ESP32 IP on serial
 
   server.on("/", handle_OnConnect); // Directamente desde e.g. 192.168.0.8
-  server.on("/led1on", handle_led1on);
-  server.on("/led1off", handle_led1off);
+//  server.on("/led1on", handle_led1on);
+//  server.on("/led1off", handle_led1off);
   
   server.onNotFound(handle_NotFound);
 
@@ -70,11 +70,11 @@ void loop() {
 
   server.handleClient();
   
-//  if (Serial2.available()>0){
-//    //read the incoming byte:
-//    Val = Serial2.read();
-//    Serial.println (Val);
-//    }  
+  if (Serial2.available()>0){
+    //read the incoming byte:
+    parqueos = Serial2.read();
+//    Serial.println (parqueos);
+    }  
 
   
   
@@ -84,61 +84,179 @@ void loop() {
 // Handler de Inicio página
 //************************************************************************************************
 void handle_OnConnect() {
-  LED1status = LOW;
-  Serial.println("GPIO2 Status: OFF");
-  server.send(200, "text/html", SendHTML(LED1status));
+  //LED1status = LOW;
+  Serial.println(parqueos);
+  server.send(200, "text/html", SendHTML(parqueos));
 }
 //************************************************************************************************
 // Handler de led1on
 //************************************************************************************************
-void handle_led1on() {
-  LED1status = HIGH;
-  Serial.println("GPIO2 Status: ON");
-  server.send(200, "text/html", SendHTML(LED1status));
-}
+//void handle_led1on() {
+//  LED1status = HIGH;
+//  Serial.println("GPIO2 Status: ON");
+//  server.send(200, "text/html", SendHTML(parqueos));
+//}
 //************************************************************************************************
 // Handler de led1off
 //************************************************************************************************
-void handle_led1off() {
-  LED1status = LOW;
-  Serial.println("GPIO2 Status: OFF");
-  server.send(200, "text/html", SendHTML(LED1status));
-}
+//void handle_led1off() {
+//  LED1status = LOW;
+//  Serial.println("GPIO2 Status: OFF");
+//  server.send(200, "text/html", SendHTML(parqueos));
+//}
 //************************************************************************************************
 // Procesador de HTML
 //************************************************************************************************
-String SendHTML(uint8_t led1stat) {
+String SendHTML(uint8_t parqueos) {
   String ptr = "<!DOCTYPE html> <html>\n";
   ptr += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
-  ptr += "<title>LED Control</title>\n";
+  ptr += "<title>PARQUEO-MATIC</title>\n";
   ptr += "<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
   ptr += "body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n";
-  ptr += ".button {display: block;width: 80px;background-color: #3498db;border: none;color: white;padding: 13px 30px;text-decoration: none;font-size: 25px;margin: 0px auto 35px;cursor: pointer;border-radius: 4px;}\n";
-  ptr += ".button-on {background-color: #3498db;}\n";
-  ptr += ".button-on:active {background-color: #2980b9;}\n";
-  ptr += ".button-off {background-color: #34495e;}\n";
-  ptr += ".button-off:active {background-color: #2c3e50;}\n";
-  ptr += "p {font-size: 14px;color: #888;margin-bottom: 10px;}\n";
+  ptr += ".button {display: block;width: 80px;background-color: #3498db;border: none;color: white;padding: 15px 30px;text-decoration: none;display: inline-block; font-size: 15px;margin: 4px auto 35px;cursor: pointer;border-radius: 2px;}\n";
+  ptr += ".button1 {background-color: #4CAF50;}\n"; /* Green */
+  ptr += ".button2 {background-color: #DC143C;}\n";/* Red */
+//  ptr += ".button1 {display: block;width: 80px;background-color: #3498db;border: none;color: white;padding: 13px 30px;text-decoration: none;font-size: 25px;margin: 0px auto 35px;cursor: pointer;border-radius: 4px;}\n";
+//  ptr += ".button-on {background-color: #3498db;}\n";
+//  ptr += ".button-on:active {background-color: #2980b9;}\n";
+//  ptr += ".button-off {background-color: #34495e;}\n";
+//  ptr += ".button-off:active {background-color: #2c3e50;}\n";
+//  ptr += "p {font-size: 14px;color: #888;margin-bottom: 10px;}\n";
   ptr += "</style>\n";
   ptr += "</head>\n";
   ptr += "<body>\n";
-  ptr += "<h1>Parqueo Matic &#128664</h1>\n";
+  ptr += "<h1>PARQUEO - MATIC &#128664</h1>\n";
   ptr += "<h3>Control de Parqueos</h3>\n";
 
-  if (led1stat)
+//  if (led1stat)
+//  {
+//    ptr += "<p>Parqueo 1:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+//    ptr += "<p>Parqueo 2:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+//    ptr += "<p>Parqueo 3:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+//    ptr += "<p>Parqueo 4:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+//  }
+//  else
+//  {
+//    ptr += "<p>Parqueo 1:</p><a class=\"button button2\">OCUPADO</a>\n";
+//    ptr += "<p>Parqueo 2:</p><a class=\"button button2\">OCUPADO</a>\n";
+//    ptr += "<p>Parqueo 3:</p><a class=\"button button2\">OCUPADO</a>\n";
+//    ptr += "<p>Parqueo 4:</p><a class=\"button button2\">OCUPADO</a>\n";
+//  }
+
+//
+    if (parqueos == 65)
   {
-    ptr += "<p>Parqueo 1: OFF</p><a class=\"button button-off\" href=\"/led1off\">OFF</a>\n";
-    ptr += "<p>Parqueo 2: OFF</p><a class=\"button button-off\" href=\"/led1off\">OFF</a>\n";
-    ptr += "<p>Parqueo 3: OFF</p><a class=\"button button-off\" href=\"/led1off\">OFF</a>\n";
-    ptr += "<p>Parqueo 4: OFF</p><a class=\"button button-off\" href=\"/led1off\">OFF</a>\n";
+    ptr += "<p>Parqueo 1:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 2:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 3:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 4:</p><a class=\"button button1\">DISPONIBLE</a>\n";
   }
-  else
+  else if (parqueos == 66)
   {
-    ptr += "<p>Parqueo 1: ON</p><a class=\"button button-on\" href=\"/led1on\">ON</a>\n";
-    ptr += "<p>Parqueo 2: ON</p><a class=\"button button-on\" href=\"/led1on\">ON</a>\n";
-    ptr += "<p>Parqueo 3: ON</p><a class=\"button button-on\" href=\"/led1on\">ON</a>\n";
-    ptr += "<p>Parqueo 4: ON</p><a class=\"button button-on\" href=\"/led1on\">ON</a>\n";
+    ptr += "<p>Parqueo 1:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 2:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 3:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 4:</p><a class=\"button button2\">OCUPADO</a>\n";
   }
+    else if (parqueos == 67)
+  {
+    ptr += "<p>Parqueo 1:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 2:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 3:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 4:</p><a class=\"button button2\">OCUPADO</a>\n";
+  }
+    else if (parqueos == 68)
+  {
+    ptr += "<p>Parqueo 1:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 2:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 3:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 4:</p><a class=\"button button2\">OCUPADO</a>\n";
+  }
+    else if (parqueos == 69)
+  {
+    ptr += "<p>Parqueo 1:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 2:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 3:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 4:</p><a class=\"button button2\">OCUPADO</a>\n";
+  }
+    else if (parqueos == 70)
+  {
+    ptr += "<p>Parqueo 1:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 2:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 3:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 4:</p><a class=\"button button2\">OCUPADO</a>\n";
+  }
+    else if (parqueos == 71)
+  {
+    ptr += "<p>Parqueo 1:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 2:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 3:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 4:</p><a class=\"button button2\">OCUPADO</a>\n";
+  }
+    else if (parqueos == 72)
+  {
+    ptr += "<p>Parqueo 1:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 2:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 3:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 4:</p><a class=\"button button2\">OCUPADO</a>\n";
+  }
+    else if (parqueos == 73)
+  {
+    ptr += "<p>Parqueo 1:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 2:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 3:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 4:</p><a class=\"button button2\">OCUPADO</a>\n";
+  }
+    else if (parqueos == 74)
+  {
+    ptr += "<p>Parqueo 1:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 2:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 3:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 4:</p><a class=\"button button2\">OCUPADO</a>\n";
+  }
+    else if (parqueos == 75)
+  {
+    ptr += "<p>Parqueo 1:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 2:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 3:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 4:</p><a class=\"button button2\">OCUPADO</a>\n";
+  }
+    else if (parqueos == 76)
+  {
+    ptr += "<p>Parqueo 1:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 2:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 3:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 4:</p><a class=\"button button2\">OCUPADO</a>\n";
+  }
+    else if (parqueos == 77)
+  {
+    ptr += "<p>Parqueo 1:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 2:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 3:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 4:</p><a class=\"button button2\">OCUPADO</a>\n";
+  }
+    else if (parqueos == 78)
+  {
+    ptr += "<p>Parqueo 1:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 2:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 3:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 4:</p><a class=\"button button2\">OCUPADO</a>\n";
+  }
+    else if (parqueos == 79)
+  {
+    ptr += "<p>Parqueo 1:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 2:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 3:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 4:</p><a class=\"button button2\">OCUPADO</a>\n";
+  }
+    else if (parqueos == 80)
+  {
+    ptr += "<p>Parqueo 1:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 2:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 3:</p><a class=\"button button1\">DISPONIBLE</a>\n";
+    ptr += "<p>Parqueo 4:</p><a class=\"button button2\">OCUPADO</a>\n";
+  }
+
 
   ptr += "</body>\n";
   ptr += "</html>\n";
--- Proyecto_4/main.c
+++ Proyecto_4/main.c
@@ -37,12 +37,20 @@
 //**************************************
 uint32_t parqueo = 0;
 
+uint32_t P1 = 0;
+uint32_t P2 = 0;
+uint32_t P3 = 0;
+uint32_t P4 = 0;
+
+char val = '0';
+
 //**************************************
 // PROTOTIPOS DE FUNCIONES
 //**************************************
 void revisar(void);
 void display_7seg(void);
-
+void datos_uart(void);
+void InitUART(void);
 
 /**
  * main.c
@@ -54,32 +62,32 @@ int main(void)
 // CONFIG
 //**************************************
     //CONGIF DE FRECUENCIA (40 MHz)
-    SysCtlClockSet(SYSCTL_SYSDIV_5 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN| SYSCTL_XTAL_16MHZ);  //FREQ - 40MHz
+    SysCtlClockSet(SYSCTL_SYSDIV_5 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN| SYSCTL_XTAL_16MHZ);  //FREQ - 40MHz
 
     //CONFIG HABILITAR PUERTOS
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
+        SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+        SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+        SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);
+        SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
+        SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
+        SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
 
-
-    //VERIFICAR HABILITACIÓN DE PUERTO
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOA)){
+    while (!SysCtlPeripheralReady (SYSCTL_PERIPH_GPIOA)){
     }
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOB)){
-    }
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOC)){
+    while (!SysCtlPeripheralReady (SYSCTL_PERIPH_GPIOB)){
         }
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOD)){
+    while (!SysCtlPeripheralReady (SYSCTL_PERIPH_GPIOC)){
+            }
+    while (!SysCtlPeripheralReady (SYSCTL_PERIPH_GPIOD)){
         }
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOE)){
+    while (!SysCtlPeripheralReady (SYSCTL_PERIPH_GPIOE)){
         }
-
+    while (!SysCtlPeripheralReady (SYSCTL_PERIPH_GPIOF)){
+        }
 
 
     //IO CONFIG
-    GPIOPinTypeGPIOOutput(GPIO_PORTC_BASE, GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7); //Indicadores Parqueos 1 y 2
+    GPIOPinTypeGPIOOutput(GPIO_PORTA_BASE, GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_6 | GPIO_PIN_7); //Indicadores Parqueo 1 y 2
     GPIOPinTypeGPIOOutput(GPIO_PORTE_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4); //Indicadores Parqueos 3 y 4
     GPIOPinTypeGPIOOutput(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7); //Indicador 7seg
 
@@ -90,7 +98,14 @@ int main(void)
     GPIOPadConfigSet(GPIO_PORTD_BASE, GPIO_PIN_2, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPD);
     GPIOPadConfigSet(GPIO_PORTD_BASE, GPIO_PIN_3, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPD);
 
+    InitUART();
 
+    P1 = 0;
+    P2 = 0;
+    P3 = 0;
+    P4 = 0;
+
+    //UARTCharPut(UART1_BASE, '1');
 
 //**************************************
 // MAIN LOOP
@@ -98,6 +113,8 @@ int main(void)
     while(1){
         revisar();
         display_7seg();
+        datos_uart();
+        UARTCharPut(UART1_BASE, val);
         }
     }
 
@@ -107,49 +124,59 @@ int main(void)
 void revisar(void)
 {
     parqueo = 0;
+    P1 = 0;
+    P2 = 0;
+    P3 = 0;
+    P4 = 0;
+
+
     if ( !GPIOPinRead(GPIO_PORTD_BASE, GPIO_PIN_0) )  { //Revisamo Parqueo 1
 
-        GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_4 | GPIO_PIN_5, GPIO_PIN_5); //Parqueo Desocupado
+        GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_6 | GPIO_PIN_7, GPIO_PIN_7); //Parqueo Desocupado
         parqueo = parqueo+1;
+        P1 = 1;
     }
 
     else if ( GPIOPinRead(GPIO_PORTD_BASE, GPIO_PIN_0) ) {
-            GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_4 | GPIO_PIN_5, GPIO_PIN_4); //Parqueo Ocupado
-
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_6 | GPIO_PIN_7, GPIO_PIN_6); //Parqueo Ocupado
+            P1 = 0;
        }
 
     if ( !GPIOPinRead(GPIO_PORTD_BASE, GPIO_PIN_1) )  { //Revisamo Parqueo 2
 
-        GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_6 | GPIO_PIN_7, GPIO_PIN_7); //Parqueo Desocupado
+        GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_2 | GPIO_PIN_3, GPIO_PIN_3); //Parqueo Desocupado
         parqueo = parqueo+1;
+        P2 = 1;
     }
 
     else if ( GPIOPinRead(GPIO_PORTD_BASE, GPIO_PIN_1) ) {
-            GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_6 | GPIO_PIN_7, GPIO_PIN_6); //Parqueo Ocupado
-
-       }
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_2 | GPIO_PIN_3, GPIO_PIN_2); //Parqueo Ocupado
+            P2 = 0;
+    }
     if ( !GPIOPinRead(GPIO_PORTD_BASE, GPIO_PIN_2) )  { //Revisamo Parqueo 3
 
         GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_1 | GPIO_PIN_2, GPIO_PIN_2); //Parqueo Desocupado
         parqueo = parqueo+1;
+        P3 = 1;
     }
 
     else if ( GPIOPinRead(GPIO_PORTD_BASE, GPIO_PIN_2) ) {
-            GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_1 | GPIO_PIN_2, GPIO_PIN_1); //Parqueo Ocupado
-
-       }
+        GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_1 | GPIO_PIN_2, GPIO_PIN_1); //Parqueo Ocupado
+        P3 = 0;
+    }
     if ( !GPIOPinRead(GPIO_PORTD_BASE, GPIO_PIN_3) )  { //Revisamo Parqueo 4
 
         GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_3 | GPIO_PIN_4, GPIO_PIN_4); //Parqueo Desocupado
         parqueo = parqueo+1;
+        P4 = 1;
     }
 
     else if ( GPIOPinRead(GPIO_PORTD_BASE, GPIO_PIN_3) ) {
             GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_3 | GPIO_PIN_4, GPIO_PIN_3); //Parqueo Ocupado
-
-       }
-
+            P4 = 0;
     }
+
+   }
 
 void display_7seg(void)
 {
@@ -158,23 +185,136 @@ void display_7seg(void)
         GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7, 0x3F);
     }
     else if (parqueo == 1) {//1 parqueo libre
-        GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7,0x06);;
+        GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7,0x06);
        }
     else if (parqueo == 2) {//2 parqueos libres
-            GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7,0x5B);;
+            GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7,0x5B);
            }
     else if (parqueo == 3) {//3 parqueos libres
-            GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7,0x4F);;
+            GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7,0x4F);
            }
     else if (parqueo == 4) {//4 parqueos libres
-            GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7,0x66);;
+            GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7,0x66);
            }
 
 
     }
 
 
+void datos_uart(void){
+    if (P1 == 0 && P2 == 0 && P3 == 0 && P4 == 0){
+        val = 'P';
+    }
+    else if (P1 == 0 && P2 == 0 && P3 == 0 && P4 == 1){
+        val = 'O';
+    }
+    else if (P1 == 0 && P2 == 0 && P3 == 1 && P4 == 0){
+        val = 'N';
+    }
+    else if (P1 == 0 && P2 == 0 && P3 == 1 && P4 == 1){
+        val = 'M';
+    }
+    else if (P1 == 0 && P2 == 1 && P3 == 0 && P4 == 0){
+        val = 'L';
+    }
+    else if (P1 == 0 && P2 == 1 && P3 == 0 && P4 == 1){
+        val = 'K';
+    }
+    else if (P1 == 0 && P2 == 1 && P3 == 1 && P4 == 0){
+        val = 'J';
+    }
+    else if (P1 == 0 && P2 == 1 && P3 == 1 && P4 == 1){
+        val = 'I';
+    }
+    else if (P1 == 1 && P2 == 0 && P3 == 0 && P4 == 0){
+        val = 'H';
+    }
+    else if (P1 == 1 && P2 == 0 && P3 == 0 && P4 == 1){
+        val = 'G';
+    }
+    else if (P1 == 1 && P2 == 0 && P3 == 1 && P4 == 0){
+        val = 'F';
+    }
+    else if (P1 == 1 && P2 == 0 && P3 == 1 && P4 == 1){
+        val = 'E';
+    }
+    else if (P1 == 1 && P2 == 1 && P3 == 0 && P4 == 0){
+        val = 'D';
+    }
+    else if (P1 == 1 && P2 == 1 && P3 == 0 && P4 == 1){
+        val = 'C';
+    }
+    else if (P1 == 1 && P2 == 1 && P3 == 1 && P4 == 0){
+        val = 'B';
+    }
+    else if (P1 == 1 && P2 == 1 && P3 == 1 && P4 == 1){
+        val = 'A';
+    }
+}
 
 
 
+void InitUART(void){
 
+    /*Enable the peripheral UART Module 1*/
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART1);
+
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_UART1)){
+    }
+
+    /*Enable the GPIO Port d*/
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);
+
+    GPIOPinConfigure(GPIO_PC4_U1RX);
+    GPIOPinConfigure(GPIO_PC5_U1TX);
+
+    // Se habilitan las interrupciones Globales
+    IntMasterEnable();
+
+    /* Make the UART pins be peripheral controlled. */
+    GPIOPinTypeUART(GPIO_PORTC_BASE, GPIO_PIN_4 | GPIO_PIN_5);
+
+    UARTDisable(UART1_BASE);
+    /* Sets the configuration of a UART. */
+    UARTConfigSetExpClk(
+            UART1_BASE, SysCtlClockGet(), 115200,
+            (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+
+    IntEnable (INT_UART1);
+
+    UARTIntEnable(UART1_BASE, UART_INT_RX | UART_INT_RT);
+    UARTEnable (UART1_BASE);
+}
+
+
+//void InitUART(void){
+//
+//    /*Enable the peripheral UART Module 1*/
+//    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+//
+//    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_UART0)){
+//    }
+//
+//    /*Enable the GPIO Port d*/
+//    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+//
+//    GPIOPinConfigure(GPIO_PA0_U0RX);
+//    GPIOPinConfigure(GPIO_PA1_U0TX);
+//
+//    // Se habilitan las interrupciones Globales
+//    IntMasterEnable();
+//
+//    /* Make the UART pins be peripheral controlled. */
+//    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+//
+//    UARTDisable(UART0_BASE);
+//    /* Sets the configuration of a UART. */
+//    UARTConfigSetExpClk(
+//            UART0_BASE, SysCtlClockGet(), 115200,
+//            (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+//
+//    IntEnable (INT_UART0);
+//
+//    UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT);
+//    UARTEnable (UART0_BASE);
+//}
